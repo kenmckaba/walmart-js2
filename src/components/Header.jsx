3fba60ca2341bd2d2f940f411fc62dcd0e57401d
kenmckaba <ken@mckaba.com>
2024-11-11 17:38:59 -0800

    moved select control on to the banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { HStack, Image, VStack } from '@chakra-ui/react'
-import walmartstar from '../assets/walmart-star.png'
+import walmartstar from '../assets/walmart-logo.png'
 import { CategoryList } from './CategoryList'
 
 function Header() {
@@ -7,12 +7,12 @@ function Header() {
     <VStack height="20" width="100%" id="Header" border="1px solid green">
       <HStack width="100%" height="100%" backgroundColor="blue" id="blue">
         <Image src={walmartstar} height="100%" marginLeft="20" />
+        <CategoryList
+          onSelected={(e) => {
+            console.log('asdfasdf', e)
+          }}
+        />
       </HStack>
-      <CategoryList
-        onSelected={(e) => {
-          console.log('asdfasdf', e)
-        }}
-      />
     </VStack>
   )
 }
