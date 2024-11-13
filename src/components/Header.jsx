298bba1f88ada906bd0ce63a8c5c0ac847442a90
kenmckaba <ken@mckaba.com>
2024-11-12 21:34:54 -0800

    onSelected when a category is selected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { HStack, Image, VStack } from '@chakra-ui/react'
 import walmartstar from '../assets/walmart-logo.png'
 import { CategoryList } from './CategoryList'
+import PropTypes from 'prop-types'
 
-export function Header() {
+export function Header({ onSelected }) {
   return (
     <VStack height="20" width="100%" id="Header" border="1px solid green">
       <HStack width="100%" height="100%" backgroundColor="blue" id="blue">
@@ -10,9 +11,14 @@ export function Header() {
         <CategoryList
           onSelected={(e) => {
             console.log('asdfasdf', e)
+            onSelected(e)
           }}
         />
       </HStack>
     </VStack>
   )
+}
+
+Header.propTypes = {
+  onSelected: PropTypes.func,
 }
--- src/components/WalmartApp.jsx
+++ src/components/WalmartApp.jsx
@@ -6,7 +6,12 @@ export const WalmartApp = () => {
   return (
     <VStack height="100vh" width="100vw" border="1px solid red " id="App">
       <Header />
-      <CategoryButtons id="catbuttons" />
+      <CategoryButtons
+        id="catbuttons"
+        onSelected={(e) => {
+          console.log('asdfasdf', e)
+        }}
+      />
       <h1>Walmart</h1>
     </VStack>
   )
